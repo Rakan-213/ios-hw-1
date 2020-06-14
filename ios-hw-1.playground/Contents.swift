325e64b47610c223d29ce39ecb3771edfae752fc
Rakan-213 <66526967+Rakan-213@users.noreply.github.com>
2020-06-14 16:24:07 +0300

    done

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.5
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,20 +38,26 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
+var firstName1 = "Nasser"
+var lastName1 = "mohamad"
+var email1 = "Nasser11@hotmail.com"
+var phoneNumber1 = "99909990"
+var age1 = 19
+var country1 = "saudi"
+var area1 = "Alsalam"
+var password1 = "Nasser2020"
 var isKuwaiti = false
 
 //Person 2
-
-
-
+var firstName2 = "Aziz"
+var lastName2 = "maz"
+var email2 = "aziz11@hotmail.com"
+var phoneNumber2 = "66654311"
+var age2 = 19
+var country2 = "kuwait"
+var area2 = "yarmouk"
+var password2 = "aziz2020"
+var isKuwaiti2 = true
 
 
 
@@ -60,8 +66,15 @@ var isKuwaiti = false
 
 
 //Person 3
-
-
+var firstName3 = "Bader"
+var lastName3 = "maz"
+var email3 = "Bader22@hotmail.com"
+var phoneNumber3 = "99909990"
+var age3 = 16
+var country3 = "kuwait"
+var area3 = "yarmouk"
+var password3 = "bader212"
+var isKuwaiti3 = true
 
 
 
@@ -82,3 +95,49 @@ var isKuwaiti = false
 
  
  */
+firstName1 != firstName2
+lastName1 != lastName2
+email1 != email2
+phoneNumber1 != phoneNumber2
+age1 == age2
+country1 != country2
+area1 != area2
+password1 != password2
+isKuwaiti != isKuwaiti2
+
+
+
+firstName2 != firstName3
+lastName2 == lastName3
+email2 != email3
+phoneNumber2 != phoneNumber3
+age2 != age3
+country2 == country3
+password2 != password3
+isKuwaiti2 == isKuwaiti3
+
+
+
+firstName1 != firstName3
+lastName1 != lastName3
+email1 != email3
+phoneNumber1 == phoneNumber3
+age1 != age3
+country1 != country3
+password1 != password3
+isKuwaiti != isKuwaiti3
+
+
+age1 == age2
+age1 != age3
+age2 != age3
+
+area1 != area2
+area1 != area3
+area2 == area3
+
+age1 > 18 && age2 > 18 && age3 < 18
+
+
+age1 != 17 && age2 != 17 && age3 != 17
+
